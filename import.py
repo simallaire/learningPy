a16646e467fe63d0b0bd3f8e32203091f27867bc
simallaire <sp.allaire93@gmail.com>
2019-03-11 21:15:46 -0400

    wut

---
+++ import.py
@@ -0,0 +1,5 @@
+#%%
+
+import mathx
+
+mathx.__builtins__
--- mathx.py
+++ mathx.py
@@ -4,15 +4,15 @@ import numpy as np
 import statistics as st
 from scipy.stats import t
 import matplotlib.pyplot as mpt
-
-class Mathx:
+#%%
+class mathx:
 
     numList = [133,295,166,175,339,124,256,126,191,280,125,198]
     moyenneS=0
     n = len(numList)
     ecartTypeS=0
     sample = True
-    
+
     def __init__(self):
         self.comp_moyenne()
         self.comp_ecarttype()
@@ -41,13 +41,13 @@ class Mathx:
         alpha = 1-confidence
         print("alpha = "+ str(alpha))
         ecartTypeX = self.ecartTypeS/(self.n)**(1/2)
-        me = np.abs(t.isf(1-alpha/2,self.n-1)) #inverse Student
+        me = t.isf(1-alpha/2,self.n-1) #inverse Student
         me = me*ecartTypeX
-        return [self.moyenneS-me,self.moyenneS+me]
+        return [self.moyenneS+me,self.moyenneS-me]
 
 
 #%%
-e = Mathx()
+e = mathx()
 
 
 
@@ -56,7 +56,7 @@ print("(1 - alpha) = "+str(e.find_Confidence(166.2408,235.0925)))
 
 
 #%%
-print(e.find_borne(0.95))
+print(e.find_borne(0.99))
 
 
 #%%
@@ -69,7 +69,11 @@ mpt.show()
 
 #%%
 
-
+# a = '\\frac{a}{b}'  #notice escaped slash
+# mpt.plot()
+# mpt.text
+# mpt.text(10.5, 10.5,'$%s$'%a)
+# mpt.show()
 
 #%%
 
